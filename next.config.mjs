cb51cae1d8804827c3bef7958451bc8281ec51be
Abhideep <itsabhideep@gmail.com>
2025-07-27 21:47:44 +0530

    test4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Add webpack alias for @ paths
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    }
+    
+    return config
+  },
+};
 
 export default nextConfig;
