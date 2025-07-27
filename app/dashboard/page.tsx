d940d30bf3ac5c5e058d30a2f9c03e4dd8f6c393
Abhideep <itsabhideep@gmail.com>
2025-07-27 21:25:42 +0530

    test3

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import type React from "react"
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import parseResumeData from '../../utils/responseToJson.js'
-import { Button } from "../../components/ui/button.jsx"
+import { Button } from "../../components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
